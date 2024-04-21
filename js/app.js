090e360fa8cfc1f0eeffac105d6b2146566fbd3e
Natalia Correal <natalia.correal@uc.cl>
2024-04-21 18:50:59 -0400

    feat: use IndexedDB

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
+import { openDB } from 'idb';
+
 const container = document.querySelector(".container")
+const buttons = document.querySelectorAll(".card--link")
+const title = document.querySelectorAll(".card--title")
+
 const coffees = [
   { name: "Perspiciatis", image: "images/coffee1.jpg" },
   { name: "Voluptatem", image: "images/coffee2.jpg" },
@@ -26,7 +31,27 @@ const showCoffees = () => {
     container.innerHTML = output
 }
   
-document.addEventListener("DOMContentLoaded", showCoffees)
+document.addEventListener("DOMContentLoaded", async () => {
+  // Set up the database
+  const db = await openDB('settings-store', 1, {
+    upgrade(db) {
+      db.createObjectStore('settings');
+    },
+  });
+
+  // If a button is clicked, save the value in IndexedDB
+  buttons.addEventListener('click', async () => {
+    console.log('Button clicked');
+    await db.put('settings', 'button', 'content');
+  });
+
+  // If a title is clicked, see the 'content' value in the console
+  title.addEventListener('click', async () => {
+    console.log(await db.get('settings', 'button'));
+  });
+
+  showCoffees
+})
   
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
