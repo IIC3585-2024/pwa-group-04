56def86eae7f9f590c1e80ac49e68e41f700f65b
Natalia Correal <natalia.correal@uc.cl>
2024-04-21 20:04:36 -0400

    feat: add IndexedDB

--- js/app.js
+++ js/app.js
@@ -1,4 +1,38 @@
-import { openDB } from 'idb';
+const db = "store";
+
+const request = indexedDB.open(db, 3);
+
+request.onerror = (event) => {
+  // Handle errors.
+};
+request.onupgradeneeded = (event) => {
+  const db = event.target.result;
+
+  // Create an objectStore to hold information about our customers. We're
+  // going to use "ssn" as our key path because it's guaranteed to be
+  // unique - or at least that's what I was told during the kickoff meeting.
+  const objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
+
+  // Create an index to search customers by name. We may have duplicates
+  // so we can't use a unique index.
+  objectStore.createIndex("name", "name", { unique: false });
+
+  // Create an index to search customers by email. We want to ensure that
+  // no two customers have the same email, so use a unique index.
+  objectStore.createIndex("email", "email", { unique: true });
+
+  // Use transaction oncomplete to make sure the objectStore creation is
+  // finished before adding data into it.
+  objectStore.transaction.oncomplete = (event) => {
+    // Store values in the newly created objectStore.
+    const customerObjectStore = db
+      .transaction("customers", "readwrite")
+      .objectStore("customers");
+    customerData.forEach((customer) => {
+      customerObjectStore.add(customer);
+    });
+  };
+};
 
 const container = document.querySelector(".container");
 const coffees = [
