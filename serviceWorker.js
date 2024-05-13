514a917588bf36bb9127f8e23a31e3f10ae9acfd
domingo2000 <dedwards@buk.cl>
2024-05-12 21:52:27 -0400

    feat: re-implemented precache strategy with all necesary valid files

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,33 @@
+const precachedResources = [
+  "",
+  "index.html",
+  "recipes.html",
+  "styles/style.css",
+  "js/app.js",
+  "js/jquery-3.7.1.min.js",
+  "js/scripts/recipeScript.js",
+  "js/ingredient/ingredient_factory.js",
+  "js/ingredient/ingredient.js",
+  "js/steps/steps_factory.js",
+  "js/recipe/index.js",
+  "js/recipe/recipe_factory.js",
+  "js/recipe/recipe_repository.js",
+  "js/recipe/recipe.js",
+  "js/firebase-initializer.js",
+  "firebase-messaging-sw.js",
+];
+
+const cacheName = "cooknote-site-v1";
+
+async function precache() {
+  const cache = await caches.open(cacheName);
+  return cache.addAll(precachedResources);
+}
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(precache());
+});
+
 function isCacheable(request) {
   const url = new URL(request.url);
   return !url.pathname.endsWith(".json");
@@ -6,7 +36,7 @@ function isCacheable(request) {
 async function cacheFirstWithRefresh(request) {
   const fetchResponsePromise = fetch(request).then(async (networkResponse) => {
     if (networkResponse.ok) {
-      const cache = await caches.open("MyCache_1");
+      const cache = await caches.open(cacheName);
       cache.put(request, networkResponse.clone());
     }
     return networkResponse;
