0699326712800beb691429b245fc6a018fb1d5d6
domingo2000 <dedwards@buk.cl>
2024-05-12 21:12:27 -0400

    fix: changed service worker cache strategy to cache first with refresh
    This new cache strategy provides better mantainance for offline mode

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,48 +1,22 @@
-const assets = [
-  ``,
-  `index.html`,
-  `recipes.html`,
-  `styles/style.css`,
-  `js/app.js`,
-  `js/jquery-3.7.1.min.js`,
-  `js/scripts/recipeScript.js`,
-  `js/ingredient/ingredient_factory.js`,
-  `js/ingredient/ingredient.js`,
-  `js/steps/steps_factory.js`,
-  `js/recipe/index.js`,
-  `js/recipe/recipe_factory.js`,
-  `js/recipe/recipe_repository.js`,
-  `js/recipe/recipe.js`,
-  `assets/images/coffee1.jpg`,
-  `assets/images/coffee2.jpg`,
-  `assets/images/coffee3.jpg`,
-  `assets/images/coffee4.jpg`,
-  `assets/images/coffee5.jpg`,
-  `assets/images/coffee6.jpg`,
-  `assets/images/coffee7.jpg`,
-  `assets/images/coffee8.jpg`,
-  `assets/images/coffee9.jpg`
-];
+function isCacheable(request) {
+  const url = new URL(request.url);
+  return !url.pathname.endsWith(".json");
+}
 
-const staticCookNote = "cooknote-site-v1";
+async function cacheFirstWithRefresh(request) {
+  const fetchResponsePromise = fetch(request).then(async (networkResponse) => {
+    if (networkResponse.ok) {
+      const cache = await caches.open("MyCache_1");
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+  });
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticCookNote).then(cache => {
-      cache.addAll(assets).catch(error => {
-        console.error("Failed to cache:", error);
-      });
-      console.log("Caching assets...")
-    })
-  );
-});
+  return (await caches.match(request)) || (await fetchResponsePromise);
+}
 
-self.addEventListener("fetch", fetchEvent => {
-  console.log("Fetching...");
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      console.log(res, "res")
-      return res || fetch(fetchEvent.request);
-    })
-  );
+self.addEventListener("fetch", (event) => {
+  if (isCacheable(event.request)) {
+    event.respondWith(cacheFirstWithRefresh(event.request));
+  }
 });
